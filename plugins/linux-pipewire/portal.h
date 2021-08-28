255bd77b9a313964091d2164b746f2258add7973
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2021-08-28 17:52:52 -0300

    pipewire: Rename some static functions
    In preparation for future changes, rename and suffle some code
    around so that retrieving the GDBusConnection is a separate
    step from retriving a D-Bus proxy.

--- plugins/linux-pipewire/portal.h
+++ plugins/linux-pipewire/portal.h
@@ -26,4 +26,4 @@
 uint32_t portal_get_available_capture_types(void);
 
 GDBusConnection *portal_get_dbus_connection(void);
-GDBusProxy *portal_get_dbus_proxy(void);
+GDBusProxy *portal_get_screencast_proxy(void);
