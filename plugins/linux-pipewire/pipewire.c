a6987c5b4e4abc4c1c3ef0e4a028b6d4ce533441
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2021-09-07 06:53:57 -0300

    linux-pipewire: Use DRM fourcc defines directly
    It was naive to add these defines here to avoid a direct include when
    the entire platform already depends on the DRM subsystem. Just include
    it and let it provide the image formats.

--- plugins/linux-pipewire/pipewire.c
+++ plugins/linux-pipewire/pipewire.c
@@ -20,6 +20,7 @@
 
 #include "pipewire.h"
 
+#include <drm/drm_fourcc.h>
 #include <fcntl.h>
 #include <glad/glad.h>
 #include <linux/dma-buf.h>
@@ -34,23 +35,6 @@
 #define CURSOR_META_SIZE(width, height)                                    \
 	(sizeof(struct spa_meta_cursor) + sizeof(struct spa_meta_bitmap) + \
 	 width * height * 4)
-
-#define fourcc_code(a, b, c, d)                                \
-	((__u32)(a) | ((__u32)(b) << 8) | ((__u32)(c) << 16) | \
-	 ((__u32)(d) << 24))
-
-#define DRM_FORMAT_XRGB8888        \
-	fourcc_code('X', 'R', '2', \
-		    '4') /* [31:0] x:R:G:B 8:8:8:8 little endian */
-#define DRM_FORMAT_XBGR8888        \
-	fourcc_code('X', 'B', '2', \
-		    '4') /* [31:0] x:B:G:R 8:8:8:8 little endian */
-#define DRM_FORMAT_ARGB8888        \
-	fourcc_code('A', 'R', '2', \
-		    '4') /* [31:0] A:R:G:B 8:8:8:8 little endian */
-#define DRM_FORMAT_ABGR8888        \
-	fourcc_code('A', 'B', '2', \
-		    '4') /* [31:0] A:B:G:R 8:8:8:8 little endian */
 
 struct _obs_pipewire_data {
 	uint32_t pipewire_node;
