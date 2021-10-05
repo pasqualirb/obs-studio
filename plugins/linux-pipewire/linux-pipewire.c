25d040b44ba7538f05a85d3d34c4dc92cf025eda
columbarius <co1umbarius@protonmail.com>
2021-10-05 02:18:50 +0200

    linux-pipewire: Create new plugin
    This plugin should contain different plugins importing and exporting
    multimedia streams via PipeWire.

---
+++ plugins/linux-pipewire/linux-pipewire.c
@@ -0,0 +1,34 @@
+/* linux-pipewire.c
+ *
+ * Copyright 2021 columbarius <co1umbarius@protonmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <obs/obs-module.h>
+#include <obs/obs-nix-platform.h>
+
+OBS_DECLARE_MODULE()
+
+bool obs_module_load(void)
+{
+	return true;
+}
+
+void obs_module_unload(void)
+{
+	// This empty line is intentional
+}
